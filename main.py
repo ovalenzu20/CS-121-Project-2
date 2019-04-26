4a4d23b0a39cbb121c8e9472f6f440a22a6fd3e2
Omar Valenzuela <ovalenzu@uci.edu>
2019-04-25 18:24:22 -0700

    added all files

---
+++ main.py
@@ -0,0 +1,22 @@
+import atexit
+import logging
+
+import lxml
+from crawler import Crawler
+from frontier import Frontier
+
+if __name__ == "__main__":
+    # Configures basic logging
+    logging.basicConfig(format='%(asctime)s (%(name)s) %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.INFO)
+
+    # Instantiates frontier and loads the last state if exists
+    frontier = Frontier()
+    frontier.load_frontier()
+    print(lxml.__name__)
+    # Registers a shutdown hook to save frontier state upon unexpected shutdown
+    atexit.register(frontier.save_frontier)
+
+    # Instantiates a crawler object and starts crawling
+    crawler = Crawler(frontier)
+    crawler.start_crawling()
